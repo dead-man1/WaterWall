d0c65e015d336bea07d1d53e071a19000e281e9b
radkesvat <134321679+radkesvat@users.noreply.github.com>
2026-04-10 16:41:35 +0000

    fix leaks some leaks when destroying config file or on some errors, add more checks

--- ww/node_builder/config_file.c
+++ ww/node_builder/config_file.c
@@ -2,25 +2,23 @@
 #include "loggers/internal_logger.h"
 #include "utils/json_helpers.h"
 
+static void freeConfigFile(config_file_t *state)
+{
+    if (state == NULL)
+    {
+        return;
+    }
+    cJSON_Delete(state->root);
+    memoryFree(state->file_path);
+    memoryFree(state->name);
+    memoryFree(state->author);
+    mutexDestroy(&(state->guard));
+    memoryFree(state);
+}
+
 void destroyConfigFile(config_file_t *state)
 {
-    cJSON_Delete(state->root);
-
-    if (state->file_path != NULL)
-    {
-        memoryFree(state->file_path);
-    }
-    if (state->name != NULL)
-    {
-        memoryFree(state->name);
-    }
-    if (state->author != NULL)
-    {
-        memoryFree(state->author);
-    }
-    mutexDestroy(&(state->guard));
-
-    memoryFree(state);
+    freeConfigFile(state);
 }
 
 void acquireUpdateLock(config_file_t *state)
@@ -36,18 +34,26 @@ void releaseUpdateLock(config_file_t *state)
 // only use if you acquired lock before
 void unsafeCommitChanges(config_file_t *state)
 {
-    char     *string      = cJSON_PrintBuffered(state->root, (int) ((state->file_prebuffer_size) * 2), true);
+    char *string = cJSON_PrintBuffered(state->root, (int) ((state->file_prebuffer_size) * 2), true);
+    if (string == NULL)
+    {
+        LOGE("WriteFile Error: cJSON_PrintBuffered failed for \"%s\"", state->file_path);
+        return;
+    }
     size_t    len         = strlen(string);
     const int max_retries = 3;
     for (int i = 0; i < max_retries; i++)
     {
         if (writeFile(state->file_path, string, len))
         {
+            state->file_prebuffer_size = len;
+            memoryFree(string);
             return;
         }
         LOGE("WriteFile Error: could not write to \"%s\". retry...", state->file_path);
     }
     LOGE("WriteFile Error: giving up writing to config file \"%s\"", state->file_path);
+    memoryFree(string);
 }
 
 void commitChangesHard(config_file_t *state)
@@ -60,15 +66,14 @@ void commitChangesHard(config_file_t *state)
 // will not write if the mutex is locked
 void commitChangesSoft(config_file_t *state)
 {
-    discard state;
 #ifdef OS_WIN
     commitChangesHard(state);
 #else
-    // if (mutexTryLock(&(state->guard)))
-    // {
-    //     unsafeCommitChanges(state);
-    //     releaseUpdateLock(state);
-    // }
+    if (mutexTryLock(&(state->guard)))
+    {
+        unsafeCommitChanges(state);
+        releaseUpdateLock(state);
+    }
 
 #endif
 }
@@ -87,14 +92,14 @@ config_file_t *configfileParse(const char *const file_path)
     if (! data_json)
     {
         LOGF("File Error: config file \"%s\" could not be read", file_path);
-        memoryFree(state->file_path);
-        memoryFree(state);
+        configfileDestroy(state);
         return NULL;
     }
     state->file_prebuffer_size = strlen(data_json);
 
     cJSON *json = cJSON_ParseWithLength(data_json, state->file_prebuffer_size);
     state->root = json;
+    memoryFree(data_json);
 
     if (json == NULL)
     {
@@ -104,17 +109,14 @@ config_file_t *configfileParse(const char *const file_path)
         {
             LOGF("JSON Error: before: %s\n", error_ptr);
         }
-        memoryFree(state->file_path);
-        memoryFree(state);
+        configfileDestroy(state);
         return NULL;
     }
-    memoryFree(data_json);
 
     if (! getStringFromJsonObject((&state->name), json, "name"))
     {
         LOGF("JSON Error: config file \"%s\" -> name (string field) the value was empty or invalid", file_path);
-        memoryFree(state->file_path);
-        memoryFree(state);
+        configfileDestroy(state);
         return NULL;
     }
 
@@ -126,10 +128,7 @@ config_file_t *configfileParse(const char *const file_path)
     if (! (cJSON_IsArray(nodes) && nodes->child != NULL))
     {
         LOGF("JSON Error: config file \"%s\" -> nodes (array field) the array was empty or invalid", file_path);
-        memoryFree(state->name);
-        memoryFree(state->author);
-        memoryFree(state->file_path);
-        memoryFree(state);
+        configfileDestroy(state);
         return NULL;
     }
     state->nodes = nodes;
@@ -138,9 +137,5 @@ config_file_t *configfileParse(const char *const file_path)
 
 void configfileDestroy(config_file_t *cf)
 {
-    cJSON_Delete(cf->root);
-    memoryFree(cf->name);
-    memoryFree(cf->author);
-    memoryFree(cf->file_path);
-    memoryFree(cf);
+    freeConfigFile(cf);
 }
