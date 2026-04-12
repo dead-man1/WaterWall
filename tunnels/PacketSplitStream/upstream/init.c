87bdf59ec6e448110c0a8a49682cd77008272ec5
radkesvat <134321679+radkesvat@users.noreply.github.com>
2026-04-12 17:49:05 +0000

    send init to both directions

--- tunnels/PacketSplitStream/upstream/init.c
+++ tunnels/PacketSplitStream/upstream/init.c
@@ -4,4 +4,5 @@ void packetsplitstreamTunnelUpStreamInit(tunnel_t *t, line_t *l)
 {
     packetsplitstream_tstate_t *ts = tunnelGetState(t);
     tunnelUpStreamInit(ts->up_tunnel, l);
+    tunnelUpStreamInit(ts->down_tunnel, l);
 }
